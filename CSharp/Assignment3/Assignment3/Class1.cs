085f3931c680ab9b9132791a633aa0b8522c6bce
Ramya Sadhu <ramyasa@infinite.com>
2022-10-02 15:13:14 +0530

    Day3 Project

---
+++ CSharp/Assignment3/Assignment3/Class1.cs
@@ -0,0 +1,73 @@
+﻿
+using System;
+
+namespace Assignment3
+{
+    class Accounts
+    {
+        public int Accno, amt, bal, Deposit, Withdrawal; 
+        public string Name, Acctype;
+        public void Credit()
+        {
+
+            Console.WriteLine("Enter the amount to deposit : "); 
+            int creditamt = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Your available balance is :{0} ", +(bal + creditamt));
+            Console.Read();
+
+        }
+
+        public void Debit() 
+        { 
+            Console.WriteLine("Enter the amount to withdrawal : "); 
+            int debitamt = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your available balance is :{0} ", +(bal - debitamt)); 
+            Console.Read();
+        }
+
+        public void Displaydetails()
+        {
+            Console.WriteLine("Account holder name is {0}", Name);
+            Console.WriteLine("Account number {0}", Accno); 
+            Console.WriteLine("account type {0}", Acctype);
+
+        }
+    }
+
+    class DriveClass
+    {
+        static void Main(string[] args)
+        {
+            Accounts program = new Accounts();
+
+            Console.WriteLine("Enter your name: "); 
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Enter your account number : "); 
+            int Accno = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your account type saving or current account : "); 
+            string Acctype = Console.ReadLine(); 
+            Console.WriteLine("Select the type of transaction type 1)Deposit 2)Withdrawal :"); 
+            string st = (Console.ReadLine());
+
+            if (st == "Deposit")
+            { 
+                program.Credit();
+            }
+            else if (st == "Withdrawal") 
+            { 
+                program.Debit(); 
+            
+            } 
+            else 
+            { 
+                Console.WriteLine("Invalid option");
+            }
+
+            program.Displaydetails();
+            Console.ReadKey();
+
+        }
+    }
+}
+
---
+++ CSharp/Assignment3/Assignment3/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+
+
+namespace Assignment3
+{
+    class Student
+    {
+
+        public int Rollno, Class, Semester, sub1, sub2, sub3, sub4, sub5;
+        public string name, branch;
+
+
+
+        public Student(string nm, int cls, int sem, string brn, int Rn)
+        {
+            Console.WriteLine("Enter the  student name : ");
+            nm = Console.ReadLine();
+            Console.WriteLine("Enter  the student class : ");
+            cls = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the semester  : ");
+            sem = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Branch : ");
+            brn = Console.ReadLine();
+            Console.WriteLine("Enter the  student Roll_Number : ");
+            Rn = Convert.ToInt32(Console.ReadLine());
+            Rollno = Rn;
+            name = nm;
+            Class = cls;
+            Semester = sem;
+            branch = brn;
+        }
+
+
+
+        public void Getmarks()
+        {
+            Console.WriteLine("Enter the marks of 5 subjects : ");
+            sub1 = Convert.ToInt32(Console.ReadLine());
+            sub2 = Convert.ToInt32(Console.ReadLine());
+            sub3 = Convert.ToInt32(Console.ReadLine());
+            sub4 = Convert.ToInt32(Console.ReadLine());
+            sub5 = Convert.ToInt32(Console.ReadLine());
+            int[] marks = new int[5];
+            marks[0] = sub1;
+            marks[1] = sub2;
+            marks[2] = sub3;
+            marks[3] = sub4;
+            marks[4] = sub5;
+
+
+
+        }
+        public void DisplayResults()
+        {
+            int sum;
+            float avg = 0.0F;
+            sum = sub1 + sub2 + sub3 + sub4 + sub5;
+            avg = (float)sum / 5;
+            if (sub1 < 35)
+            {
+                Console.WriteLine("student is failed ");
+            }
+            else if (sub2 < 35)
+            {
+                Console.WriteLine("Student is failed ");
+            }
+            else if (sub3 < 35)
+            {
+                Console.WriteLine("Student is failed ");
+            }
+            else if (sub4 < 35)
+            {
+                Console.WriteLine("Student is failed ");
+            }
+            else if (sub5 < 35)
+            {
+                Console.WriteLine("Student is failed ");
+            }
+            else if (sum > 35 && avg < 50)
+            {
+                Console.WriteLine("Student is failed ");
+            }
+            else if (avg > 50)
+            {
+                Console.WriteLine("Student is passed");
+                Console.Read();
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Student student = new Student("", 0, 0, "", 0);
+            student.Getmarks();
+            student.DisplayResults();
+            Console.ReadLine();
+         }
+    }
+}
